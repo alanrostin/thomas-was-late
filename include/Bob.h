3d073934b27d03799cc80aee616f76b1898f4638
alanrostin <alan.rostin@gmail.com>
2021-06-29 13:12:21 +0430

    Modify update and draw function for characters

--- include/Bob.h
+++ include/Bob.h
@@ -5,7 +5,7 @@ class Bob : public PlayableCharacter
 {
     public:
         // A constructor specific to Bob
-        Bob::Bob();
+        Bob();
 
         // Tho overridden input handler for Bob
         bool virtual handleInput();
