a55b9e4f34d400b681313a9e0c14d64e29ef5c4b
alanrostin <alan.rostin@gmail.com>
2021-07-01 13:39:45 +0430

    Add detectCollisions function

--- include/Engine.h
+++ include/Engine.h
@@ -79,6 +79,8 @@ class Engine
         // Load a new level
         void loadLevel();
 
+        bool detectCollisions(PlayableCharacter& character);
+
     public:
         // The Engine constructor
         Engine();
